8965ad83b9c765591a4bafc825274ae06cd62414
Szerlock <ryan.cubas21@gmail.com>
2024-08-25 16:48:11 -0600

    Updated
    Implemented a bit of UI components

---
+++ Nun 3D Shooter/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject startMenu;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0f;
+
+    }
+
+    // Update is called once per frame
+    public void StartGame()
+    {
+        // Hide the start menu
+        if (startMenu != null)
+        {
+            startMenu.SetActive(false);
+        }
+
+        // Unpause the game
+        Time.timeScale = 1f;
+    }
+}
